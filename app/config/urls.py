31e2f8e6fd28777ab2424c51f8469869f508e3b3
Mauricio Roh <mauriroh@gmail.com>
2023-09-15 13:36:34 -0300

    login and logout

--- app/config/urls.py
+++ app/config/urls.py
@@ -16,11 +16,23 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 from core.homepage.views import IndexView
+from core.login.views import *
 
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('admin/', admin.site.urls),
+    path('login/', include('core.login.urls')),
     path('erp/', include('core.erp.urls')),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# handler404 = page_not_found404
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
